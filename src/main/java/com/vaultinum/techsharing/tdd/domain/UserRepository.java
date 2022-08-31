14d45805a9642d2dd59fef4ca0a172f0235d3576
Benjamin PREVOT <bprevot@vaultinum.com>
2022-08-31 08:48:05 +0200

    feat: Manage user loading by id

---
+++ src/main/java/com/vaultinum/techsharing/tdd/domain/UserRepository.java
@@ -0,0 +1,7 @@
+package com.vaultinum.techsharing.tdd.domain;
+
+public interface UserRepository {
+
+    User getById(String id);
+
+}
---
+++ src/main/java/com/vaultinum/techsharing/tdd/domain/UserService.java
@@ -0,0 +1,17 @@
+package com.vaultinum.techsharing.tdd.domain;
+
+import java.util.Optional;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> getById(String id) {
+        return Optional.of(userRepository.getById(id));
+    }
+
+}
