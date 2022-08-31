83587b2f50f9ea0474d8e87a020026e8c4fc1abc
Benjamin PREVOT <bprevot@vaultinum.com>
2022-08-31 09:05:09 +0200

    test: Check users list loading

--- src/test/java/com/vaultinum/techsharing/tdd/domain/UserServiceTest.java
+++ src/test/java/com/vaultinum/techsharing/tdd/domain/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.vaultinum.techsharing.tdd.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -42,6 +43,22 @@ class UserServiceTest {
         Optional<User> actualUser = userService.getById("id-for-absent-user");
 
         assertThat(actualUser.isEmpty(), is(true));
+    }
+
+    @Test
+    public void shouldReturnAllUsers() {
+        List<User> expectedUsers = List.of(
+                new User("ad358f5c-c978-49f2-9276-10e7f14bb06b", "James", "HETFIELD"),
+                new User("93778b59-96af-422d-8a48-71b0d4af0c23", "Lars", "ULRICH"),
+                new User("1518e234-a50b-4916-a396-563f22497fb6", "Kirk", "HAMMETT"),
+                new User("d10100aa-4eb0-4695-b8a1-ee21a37d5e82", "Robert", "TRUJILLO")
+        );
+
+        when(userRepository.findAll()).thenReturn(expectedUsers);
+
+        List<User> actualUsers = userService.findAll();
+
+        assertThat(actualUsers, is(equalTo(expectedUsers)));
     }
 
 }
