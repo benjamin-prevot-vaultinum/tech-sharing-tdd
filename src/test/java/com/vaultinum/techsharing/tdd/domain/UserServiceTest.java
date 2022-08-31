fab0f332d527dc96fae9d380a656c0006248bfe1
Benjamin PREVOT <bprevot@vaultinum.com>
2022-08-31 08:44:00 +0200

    test: Check user result if exists

--- src/test/java/com/vaultinum/techsharing/tdd/domain/UserServiceTest.java
+++ src/test/java/com/vaultinum/techsharing/tdd/domain/UserServiceTest.java
@@ -1,5 +1,27 @@
 package com.vaultinum.techsharing.tdd.domain;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class UserServiceTest {
+
+    @Test
+    public void shouldGetUserById() {
+        User expectedUser = new User("24075eb0-a87b-4838-816f-8e810bc6d4c4", "John", "DOE");
+
+        UserRepository userRepository = mock(UserRepository.class);
+        when(userRepository.getById("24075eb0-a87b-4838-816f-8e810bc6d4c4")).thenReturn(expectedUser);
+
+        Optional<User> actualUser = new UserService(userRepository).getById("24075eb0-a87b-4838-816f-8e810bc6d4c4");
+
+        assertThat(actualUser.get(), is(equalTo(expectedUser)));
+    }
 
 }
